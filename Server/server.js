8fbc4b5482d4c73c040f57f3e4e9c0bc861d4a30
Kofi Boateng <kdboat2@gmail.com>
2022-06-17 21:40:57 -0500

    First commit

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,6 @@
 "use strict";
 require("dotenv").config();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 const express = require("express");
 const logger = require("morgan");
 const app = express();
